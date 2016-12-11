7804b42d0ab9bf172a443b23bf47dba9048fb575
Marcos Carro <macarrfer@gmail.com>
2016-12-11 17:25:47 +0100

    Modified README.md
    Added measured times to readme

--- src/main.c
+++ src/main.c
@@ -3,16 +3,28 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "tabuSearch.h"
+#include <sys/time.h>
 
 int main(int argc, char const *argv[]) {
+struct timeval firstTime, lastTime;
+double totalTime,durationGetTime;
+
   switch (argc){
     case 2:
+    gettimeofday(&firstTime,NULL);
+    gettimeofday(&lastTime,NULL);
+    durationGetTime = (lastTime.tv_sec-firstTime.tv_sec+(lastTime.tv_usec-firstTime.tv_usec)/1.e6);
+    gettimeofday(&firstTime,NULL);
     initWithoutRandom(argv[1]);
     generateGreedyInitialSolution();
     // generateInitialSolution();
     run();
+    gettimeofday(&lastTime,NULL);
+    totalTime=(lastTime.tv_sec-firstTime.tv_sec+(lastTime.tv_usec-firstTime.tv_usec)/1.e6)-durationGetTime;
+    printf("Total time: %lfs\n",totalTime);
     break;
     case 3:
+
     initWithRandom(argv[1],argv[2]);
     generateInitialSolution();
     //generateInitialSolution();
